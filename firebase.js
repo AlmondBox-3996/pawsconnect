86afb5f7b9d3ee8806ca4c7367ae24398bb344bd
AlmondBox-3996 <110975662+AlmondBox-3996@users.noreply.github.com>
2023-06-03 23:24:15 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyBynx0GiVuYLYTTL699q_1JLct0tbXPJ7M",
+    authDomain: "pawsconnect-58b32.firebaseapp.com",
+    projectId: "pawsconnect-58b32",
+    storageBucket: "pawsconnect-58b32.appspot.com",
+    messagingSenderId: "744542661673",
+    appId: "1:744542661673:web:33e2badae57ff52a5a8038",
+    measurementId: "G-CLNFEEY3QH"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
---
+++ footer.js
@@ -0,0 +1,10 @@
+fetch('footer.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_footer");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
+
+{/* <script id="replace_with_footer" src="Assets/js/footer.js"></script> */}
